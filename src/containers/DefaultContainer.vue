7cb0149c20306d22d5ef8b5c7863504036b2388f
turinglabsorg <turinglabs@tuta.io>
2019-07-27 08:50:14 +0200

    coreui template and first dashboard draft

---
+++ src/containers/DefaultContainer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app">
+    <div class="header">
+      Cryptocurrencies:  100  •  Markets:  777  •  Market Cap:  $777,777,777  •  24h Vol:  $777,777
+    </div>
+    <div class="app-body">
+      <main class="main">
+        <div class="container-fluid">
+          <router-view></router-view>
+        </div>
+      </main>
+    </div>
+    <TheFooter>
+      <div>
+        KomodoPlatform
+      </div>
+    </TheFooter>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DefaultContainer',
+  components: {
+
+  },
+  data () {
+    return {
+      
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style>
+  .main{
+    margin-top:10px
+  }
+  .header{
+    font-size:12px;
+    padding:10px;
+  }
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h2>
+            <img class="m-top-30 logo" src="../../public/logo_w.svg" height="30"> 
+            Antara Market Cap
+            <b-form-input class="search" placeholder="Search"></b-form-input>
+        </h2>
+        <hr>
+        <b-table class="f-white" striped hover :items="items"></b-table>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'dashboard',
+  components: {},
+  data: function () {
+    return {
+        items: [
+          { name: 'Komodo', market_cap: '$777,777,777', price: '777USD', vol_24h: '777,777USD', last_notarization: '2 min ago' }
+        ]
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+    .m-top-30{
+        margin-top:20px
+    }
+    .f-white, .table-hover tbody tr:hover{
+        color:#fff!important
+    }
+    .logo{
+        float:left;
+        margin-right:20px;
+        margin-top:2px;
+    }
+    .search{
+        float:right;
+        width:200px;
+        margin-top:0px;
+    }
+</style>
